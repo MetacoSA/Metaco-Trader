8368bdf7f1e5f08255477522a41104daa08bce69
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-02-13 15:06:08 +0100

    fix maximize

---
+++ PowerWallet/Controls/NiceWindow.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PowerWallet.Controls
+{
+    public class NiceWindow : Window
+    {
+        static NiceWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NiceWindow), new FrameworkPropertyMetadata(typeof(NiceWindow)));
+        }
+        public NiceWindow()
+        {
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            ((Button)GetTemplateChild("PART_Min")).Click += Minimize;
+            ((Button)GetTemplateChild("PART_Max")).Click += Maximize;
+            _Max = ((Button)GetTemplateChild("PART_Max"));
+            UpdateMaxButton(false);
+            ((Button)GetTemplateChild("PART_Close")).Click += Close;
+            ((Grid)GetTemplateChild("PART_Top")).MouseDown += NiceWindow_MouseDown;
+        }
+
+
+        Button _Max;
+
+        void NiceWindow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    DragMove();
+            }
+            if (e.ClickCount == 2)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    ToggleWindowsState();
+            }
+        }
+
+        void Minimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+        void Maximize(object sender, RoutedEventArgs e)
+        {
+            ToggleWindowsState();
+        }
+
+        private void ToggleWindowsState()
+        {
+            if (_Max == null)
+                return;
+            if (this.WindowState == System.Windows.WindowState.Normal)
+            {
+                this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+                this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+                this.WindowState = System.Windows.WindowState.Maximized;
+                UpdateMaxButton(true);
+            }
+            else
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+                UpdateMaxButton(false);
+            }
+        }
+
+        private void UpdateMaxButton(bool maximized)
+        {
+            if (_Max == null)
+                return;
+            Show(_Max, "RestorePath", maximized);
+            Show(_Max, "MaximisePath", !maximized);
+        }
+
+        private void Show(Button button, string childName, bool value)
+        {
+            var child = ((FrameworkElement)button.FindName(childName));
+            if (child != null)
+                child.Visibility = value ? Visibility.Visible : System.Windows.Visibility.Hidden;
+        }
+        void Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- PowerWallet/MainWindow.xaml.cs
+++ PowerWallet/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PowerWallet.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace PowerWallet
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NiceWindow
     {
         public MainWindow()
         {
