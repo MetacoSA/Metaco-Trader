587299832b2dc8dd66a4b0c466a682c5e38935fd
Nicolas DORIER <nicolas.dorier@gmail.com>
2015-02-15 03:48:47 +0100

    missing file

---
+++ PowerWallet/Controls/ReadOnlyTextEditor.cs
@@ -0,0 +1,48 @@
+﻿using ICSharpCode.AvalonEdit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Xceed.Wpf.Toolkit;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
+namespace PowerWallet.Controls
+{
+    public class ReadOnlyTextEditor : TypeEditor<AutoSelectTextBox>
+    {
+
+        protected override AutoSelectTextBox CreateEditor()
+        {
+            AutoSelectTextBox textBox = new AutoSelectTextBox();
+            textBox.IsReadOnly = true;
+            textBox.AutoSelectBehavior = AutoSelectBehavior.OnFocus;
+            textBox.ContextMenu = new ContextMenu();
+            textBox.InputBindings.Add(new InputBinding(NavigationCommands.Search, NavigationCommands.Search.InputGestures[0]));
+            textBox.ContextMenu.Items.Add(new MenuItem()
+            {
+                Command = NavigationCommands.Search
+            });
+            textBox.ContextMenu.Items.Add(new MenuItem()
+            {
+                Command = ApplicationCommands.Cut,
+            });
+            textBox.ContextMenu.Items.Add(new MenuItem()
+            {
+                Command = ApplicationCommands.Copy,
+            });
+            textBox.ContextMenu.Items.Add(new MenuItem()
+            {
+                Command = ApplicationCommands.Paste,
+            });
+            return textBox;
+        }
+        protected override void SetValueDependencyProperty()
+        {
+            base.ValueProperty = AutoSelectTextBox.TextProperty;
+        }
+    }
+}
---
+++ PowerWallet/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,79 @@
+/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:ViewModelLocator xmlns:vm="clr-namespace:PowerWallet"
+                           x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+
+  You can also use Blend to do all this with the tool's support.
+  See http://www.galasoft.ch/mvvm
+*/
+
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace PowerWallet.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<CoinsViewModel>();
+            SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<RapidBaseClientFactory>();
+        }
+
+        public MainViewModel Main
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        public RapidBaseClientFactory RapidBaseClientFactory
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<RapidBaseClientFactory>();
+            }
+        }
+        
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+
+        public CoinsViewModel Coins
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<CoinsViewModel>();
+            }
+        }
+    }
+}
